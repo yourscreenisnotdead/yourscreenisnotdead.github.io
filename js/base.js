b8ab10a6b7a1066ded5a5a1977fd3710fff51400
Jeffrey Herrera <herrera.jeffrey@gmail.com>
2015-08-10 19:07:45 -0400

    Mobile Menu

--- js/base.js
+++ js/base.js
@@ -1,4 +1,7 @@
 $(window).load(function(){
   $( 'audio' ).audioPlayer();
   $( '.load' ).addClass('load-finished');
+  $( '.border-menu' ).click(function(){
+    $( '.mobile-nav' ).toggleClass('menu-open');
+  })
 });
