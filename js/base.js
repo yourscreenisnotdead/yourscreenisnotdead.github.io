4e2a89aede4d31d720462faa71ca4e62cc4bb013
Jeffrey Herrera <herrera.jeffrey@gmail.com>
2016-01-30 08:23:45 -0500

    Episode 17 & VHS Effect

--- js/base.js
+++ js/base.js
@@ -1,7 +1,21 @@
 $(window).load(function(){
-  $( 'audio' ).audioPlayer();
   $( '.load' ).addClass('load-finished');
   $( '.border-menu' ).click(function(){
     $( '.mobile-nav' ).toggleClass('menu-open');
   })
+  // parallax();
+});
+
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  $('.hero-title').css({
+    'transform' : 'translateY('+ wScroll /15 +'%)'
+  })
+
+  $('.hero-text').css({
+    'transform' : 'translateY('+ wScroll /20 +'%)'
+  })
+
 });
